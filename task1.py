5248367561da3ad02911881f48ad7e372662575e
gauravdal <gaurav.bamania@dal.ca>
2019-02-06 17:38:42 -0400

    updating the dictionary

--- task1.py
+++ task1.py
@@ -5,7 +5,16 @@ def street_info():
 	for line in fout:
 		line =  line.split(",")
 		tuple_list.append((line[2],line[4],line[7],line[8]))
-	
 
+	#print(tuple_list)
+	return tuple_list
 
 street_info()
+
+def type_maintenance():
+	maintenance_dict = dict()
+	fout = open("Street_Centrelines.csv","r")
+	fout.readline()
+	for line in  fout:
+		line = line.split(",")
+		maintenance_dict.setdefault(line[12],0) + 1
